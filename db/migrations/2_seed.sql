a3d3dd9ea584b5c5d147f0a0bc8a17e9462e3130
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-01 23:59:24 +0100

    add seed test data - WIP

---
+++ db/migrations/2_seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO users VALUES
+(1,"Chris","password");
+
+INSERT INTO habits VALUES
+(1,1,"Weight Lifting","2 days");
+
+INSERT INTO habit_instances VALUES
+(1,1,FALSE,01/06/2022 18:00:00);
