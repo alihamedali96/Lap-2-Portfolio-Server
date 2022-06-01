2ce692d4391c4d1869d2783d8140912ba59a3dd4
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-02 00:13:50 +0100

    add create date field to tables

---
+++ db/migrations/1_setup.sql
@@ -0,0 +1,27 @@
+DROP TABLE users;
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    password varchar(50) NOT NULL,
+    create_date timestamp NOT NULL
+);
+
+DROP TABLE habits;
+
+CREATE TABLE habits (
+    id serial PRIMARY KEY,
+    user_id int NOT NULL,
+    habit_name varcar(255) NOT NULL,
+    frequency interval,
+    create_date timestamp
+);
+
+DROP TABLE habit_instances;
+
+CREATE TABLE habit_instances (
+    id serial PRIMARY KEY,
+    habit_id int NOT NULL,
+    completed boolean,
+    create_date timestamp
+);
