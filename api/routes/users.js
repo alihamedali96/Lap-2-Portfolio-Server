2c5bce3eab5b9c3e03389f05a4c1b1fa9be6426d
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-06 18:35:13 +0100

    update path to controllers in routes.js, add create route

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router()
-const userControllers = require('../controllers/User')
+const userControllers = require('../controllers/users')
 
 
 router.get('/', userControllers.index);
 router.get('/:id', userControllers.show);
+router.post('/', userControllers.create);
 
 module.exports = router;
