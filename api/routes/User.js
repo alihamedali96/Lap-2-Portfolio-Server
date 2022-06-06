7eeb42b3bf2482a39adea2c29a29a866b9493cc5
n451m <nasim.a.bilal@gmail.com>
2022-06-06 15:20:34 +0100

    wip

--- api/routes/User.js
+++ api/routes/User.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router()
 const userControllers = require('../controllers/User')
 
+
+router.get('/', userControllers.index);
+router.get('/:id', userControllers.show);
+
 module.exports = router;
