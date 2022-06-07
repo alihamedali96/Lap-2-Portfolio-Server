cbb693a27a6528d33da1aa06de4d87e83f25ff82
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-07 16:53:23 +0100

    fix issues with create user route, added new columns

--- api/models/User.js
+++ api/models/User.js
@@ -4,7 +4,7 @@ module.exports = class User {
         this.id = data.id;   
         this.name = data.name;
         this.userName = data.userName;
-        this.email = date.email;
+        this.email = data.email;
         this.password = data.password;
         this.create_date = data.create_date;
 
@@ -45,17 +45,26 @@ module.exports = class User {
         })   
     };
 
-    static create(name,password){
+    static create(data){
         return new Promise (async (resolve, reject) => {
             try {
-                const userExistsTest = await db.query(`SELECT count(*) FROM users WHERE name = ($1)`, [name])
+               
+                console.log("data",data);
+                const { name , username, email , password } = data
+                console.log(name,username,email,password);
+
+                const usernameExistsTest = await db.query(`SELECT count(*) FROM users WHERE username = ($1)`, [username])
                 // console.log("userExistsTest",userExistsTest.rows[0].count > 0)
-                if (userExistsTest.rows[0].count > 0){
+                if (usernameExistsTest.rows[0].count > 0){
                     throw new Error('Username already exists')
                 }
+
                 const timestamp = Date.now();
                 const create_date = new Date(timestamp).toLocaleString();
-                let userData = await db.query(`INSERT INTO users (name,password,create_date) VALUES ($1, $2, $3) RETURNING *;`, [ name,password,create_date ]);
+
+                let userData = await db.query(`
+                INSERT INTO users (name,username,email,password,create_date) VALUES ($1, $2, $3, $4, $5) RETURNING *;`, [ name,username,email,password,create_date ]);
+
                 let newUser = new User(userData.rows[0]);
                 resolve (newUser);
             } catch (err) {
