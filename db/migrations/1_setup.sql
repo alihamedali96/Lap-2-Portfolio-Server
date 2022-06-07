09ebc717f8f75f8f8f1491251c7526d5eedaee96
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-07 14:45:46 +0100

    add username, email to user table schema

--- db/migrations/1_setup.sql
+++ db/migrations/1_setup.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id serial PRIMARY KEY,
     name varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
     password varchar(50) NOT NULL,
     create_date timestamp NOT NULL
 );
