106d2a0b71c18046d7f374e5514a9d9f80e2bbfe
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-06-07 16:26:20 +0100

    fix route again

--- api/routes/habits.js
+++ api/routes/habits.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router()
-const habitsControllers = require('../controllers/habits')
+const habitsControllers = require('../controllers/Habits')
 
 
 router.get('/', habitsControllers.index);
