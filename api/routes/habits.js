b8338087fd96c5a4099ad2cf905e8c66e475d67b
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-09 12:14:36 +0100

    add and edit create habit route

--- api/routes/habits.js
+++ api/routes/habits.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const router = express.Router()
-const habitsControllers = require('../controllers/Habits')
+const router = express.Router();
+const habitsControllers = require("../controllers/Habits");
 
-
-router.get('/', habitsControllers.index);
-router.get('/:id', habitsControllers.show);
-router.get('/user/:id', habitsControllers.findAllHabitsByUserId);
-
+router.get("/", habitsControllers.index);
+router.get("/:id", habitsControllers.show);
+router.get("/user/:id", habitsControllers.findAllHabitsByUserId);
+router.post("/", habitsControllers.create);
 module.exports = router;
